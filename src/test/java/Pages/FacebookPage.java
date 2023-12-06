a6a4a459a1be27dcc7cf8a719799d9678f8ae4e5
fcemyuksel <fcemyuksel@gmail.com>
2023-12-06 10:45:52 +0300

    facebook page

--- src/test/java/Pages/FacebookPage.java
+++ src/test/java/Pages/FacebookPage.java
@@ -1,5 +1,4 @@
 package Pages;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -8,7 +7,9 @@ import utilities.Driver;
 public class FacebookPage {
 
     public FacebookPage(){
+
         PageFactory.initElements(Driver.getDriver(),this);
+
     }
 
     @FindBy(xpath = "//input[@id='email']")
@@ -18,11 +19,19 @@ public class FacebookPage {
     public WebElement sifreKutusu;
 
     @FindBy(xpath = "//button[@name='login']")
-    public WebElement girisButonu;
+    public WebElement girisYapButonu;
 
     @FindBy(xpath = "//*[@title='Allow all cookies']")
     public  WebElement cookieKabulButonu;
 
     @FindBy(xpath = "//div[@class='_9ay7']")
     public WebElement girisYapilamadiElementi;
+
+    @FindBy(xpath = "//*[@class='_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy']")
+    public WebElement kayitOlusturButonu;
+
+    @FindBy(name = "websubmit")
+    public WebElement kaydolButonu;
+
+
 }
